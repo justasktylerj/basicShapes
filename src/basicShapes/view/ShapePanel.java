819558a6e2af0d2fdcf80c11348639d5d15a7306
Tyler jarrard <tjar2074@740s-pl-104.local>
2016-04-22 09:55:41 -0600

    shapes

--- src/basicShapes/view/ShapePanel.java
+++ src/basicShapes/view/ShapePanel.java
@@ -1,5 +1,31 @@
 package basicShapes.view;
 
-public class ShapePanel {
+import java.awt.*;
+import java.util.ArrayList;
 
+import javax.swing.*;
+
+import basicShapes.controller.*;
+import basicShapes.view.*;
+
+public class ShapePanel extends JPanel
+{
+	private ArrayList<Rectangle> rectangleList;
+	
+	public ShapePanel();
+	{
+		rectangleList = new ArrayList<Rectangle>();
+	}
+	
+	public void addRectangle()
+	{
+		rectangleList.add(new Rectangle(10, 20, 50, 10));
+	}
+	
+	@Override
+	protected void paintComponent(Graphics currentGraphics)
+	{
+		
+	}
 }
+
