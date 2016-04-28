319803b791d07682a8e942c96985fb56dfba6652
Tyler jarrard <tjar2074@740s-pl-104.local>
2016-04-28 09:55:53 -0600

    as

---
+++ src/basicShapes/view/GraphPanel.java
@@ -0,0 +1,23 @@
+package basicShapes.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GraphPanel extends JPanel
+{
+	private int [] graphSource;
+	
+	public GraphPanel()
+	{
+		
+	}
+	
+	protected void paintComponent(Graphics currentGraphics)
+	{
+		super.paintComponent(currentGraphics);
+		for(int index = 0; index < graphSource.length; index++)
+		{
+			
+		}
+	}
+}
