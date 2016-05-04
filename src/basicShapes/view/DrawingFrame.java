d04fc558addddcb09c9fa1850421621a08abeb0a
Tyler jarrard <tjar2074@740s-pl-104.local>
2016-05-04 07:44:19 -0600

    graph

--- src/basicShapes/view/DrawingFrame.java
+++ src/basicShapes/view/DrawingFrame.java
@@ -9,18 +9,20 @@ public class DrawingFrame extends JFrame
 {
 	private DrawingController baseController;
 	private DrawingPanel basePanel;
+	private GraphPanel graphPanel;
 	
 	public DrawingFrame(DrawingController baseController)
 	{
 		this.baseController = baseController;
 		basePanel = new DrawingPanel(baseController);
+		graphPanel = new GraphPanel();
 		
 		setupFrame();
 	}
 	
 	private void setupFrame()
 	{
-		this.setContentPane(basePanel);  //installs the panel in the frame
+		this.setContentPane(graphPanel);  //installs the panel in the frame
 		this.setSize(800,800);
 		this.setTitle("Art app!"); // find a good size for app
 		this.setResizable(false); //can't change size of window
